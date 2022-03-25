9d46d7ee23ad2cce9cc9171625678f903b65e97c
SiddharthaDvara <102278200+SiddharthaDvara@users.noreply.github.com>
2022-03-25 09:07:27 +0530

    Update menu.py

--- menu.py
+++ menu.py
@@ -4,9 +4,9 @@ import streamlit as st
 import pandas as pd
 #
 from io import BytesIO
-subprocess.check_call([sys.executable, '-m', 'pip', 'install','scikit-learn'])
-subprocess.check_call([sys.executable, '-m', 'pip', 'install','openpyxl'])
-subprocess.check_call([sys.executable, '-m', 'pip', 'install','xlsxwriter'])
+#subprocess.check_call([sys.executable, '-m', 'pip', 'install','scikit-learn'])
+#subprocess.check_call([sys.executable, '-m', 'pip', 'install','openpyxl'])
+#subprocess.check_call([sys.executable, '-m', 'pip', 'install','xlsxwriter'])
 
 from sklearn.impute import KNNImputer
 from outlier import detect_outliers
