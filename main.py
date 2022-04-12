fd093d51b7cb3b2115d68e3ffca7baa7c63e6b39
SiddharthaDvara <102278200+SiddharthaDvara@users.noreply.github.com>
2022-04-12 11:44:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -71,9 +71,9 @@ if uploaded_files :
                 rep = df[t].mean()
             if Outlier_menu == "Median":
                 rep = df[t].median()
-            
-             for c in p:
-                 df.replace(to_replace=c, value=rep,inplace=True)
+            if len(p)!=0:
+                for c in p:
+                    df.replace(to_replace=c, value=rep,inplace=True)
 
     if feature_scaling:
         spin()
